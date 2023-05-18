132fdccc899cfc0f1d53741452a1c6da4d678be3
Harwin14 <agungharwinn@gmail.com>
2023-05-18 20:40:01 +0700

    r

---
+++ test.js
@@ -0,0 +1,89 @@
+// const myNumbers = [1, 2, 3, 4, 5, 6, 7];
+// const myFunction = (arr) => {
+//     return arr.map((x) => x + 3).filter((x) => x < 7);
+// };
+// console.log(myFunction(myNumbers));
+
+// const arr1 = [2, 4, 6];
+// const arr2 = [3, 5, 7];
+
+// console.log([...arr1, ...arr2]);
+
+// const lion =1
+// let tiger =2
+// var bear
+
+// ++lion
+// bear += lion + tiger
+// tiger++
+
+// var a = ['a','b','c']
+// a[100] = ['d']
+
+// console.log(a.length);
+
+// let rain = ['ama','bor','cqrr','co']
+
+// rain.splice(0,2)
+
+// console.log(rain);
+
+// var obj
+// console.log(obj);
+
+// let animal = ['jag','eag']
+
+// animal.reverse()
+// console.log(animal.pop());
+
+// let cat ={type:'tiger',size :'large'}
+// let json = JSON.stringify(cat)
+
+// console.log(json);
+
+// let rain = 10;
+// let animal = 0;
+
+// while (rain < 13 || animal <= 2) {
+//     rain++;
+//     animal += 2;
+// }
+// console.log(animal);
+
+
+// let animal =['eag','osp','sal']
+
+// let key = animal => animal === 'sal'
+
+
+// if(animal.some(key)){
+//     console.log('swim');
+// }
+
+
+// console.log('I');
+
+
+// setTimeout(()=>{
+//     console.log('love');
+// }, 0)
+
+// console.log('java');
+
+
+// var sound = 'grunt'
+// var bear ={sound:'roar'}
+
+
+// function roar(){
+//     console.log(this.sound);
+// }
+// roar.bind(bear)
+
+
+let vowels = 'aeiou'
+
+for (let i = 0; i < vowels.length; i++) {
+
+console.log(vowels[i]);
+}
